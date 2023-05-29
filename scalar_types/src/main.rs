421bce652e5e8b8d1d16d685ff61883bc8e624c6
Don-Cryptus <don.cryptus@gmail.com>
2023-05-29 18:48:15 +0200

    scalar types

---
+++ scalar_types/src/main.rs
@@ -0,0 +1,16 @@
+#[allow(unused_variables)]
+#[allow(unused_assignments)]
+
+fn main() {
+    let pi: f32 = 4.0;
+    let million = 1_000_000;
+    println!("{}", million);
+
+    let is_day = true;
+    let is_night = false;
+    println!("{}", is_day);
+
+    let char1 = 'A';
+    let char1 = '\u{1F601}';
+    println!("{}", char1);
+}
