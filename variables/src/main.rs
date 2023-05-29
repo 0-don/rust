5624419c432ac4e62c8837172a5392f79f2f4c5a
Don-Cryptus <don.cryptus@gmail.com>
2023-05-29 18:41:11 +0200

    variables

---
+++ variables/src/main.rs
@@ -0,0 +1,20 @@
+#[allow(unused_variables)]
+#[allow(unused_assignments)]
+
+fn main() {
+    let name = "Alex";
+    let mut age = 32;
+
+    let amount: i64 = 938759274384745;
+
+    age = 33;
+
+    println!("{}", age);
+
+    let color = "blue";
+    let color = 86;
+
+    println!("{}", color);
+
+    let (a, b, c) = (43, 85, "red");
+}
