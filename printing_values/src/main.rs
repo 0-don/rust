346f14a97d7d3cc33056c9f9fa1373a8b9dc147c
Don-Cryptus <don.cryptus@gmail.com>
2023-05-29 18:26:18 +0200

    printing

--- printing_values/src/main.rs
+++ printing_values/src/main.rs
@@ -3,4 +3,7 @@ fn main() {
     println!("My name is {} and I'm {} years old", "John", 21);
     println!("a + b = {}", 3 + 9);
     println!("{0} has a {2} and {0} has a {1}", "John", "dog", "cat");
+    println!("{name} {surname}", surname = "Smith", name = "Alex");
+    println!("binary: {:b}, hex: {:x}, octal: {:o}", 10, 10, 10);
+    println!("array: {:?}", [1, 2, 3]);
 }
