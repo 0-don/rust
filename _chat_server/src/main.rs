aea6bf7acd5d138e7f13bfd6d7dd941af77fd325
Don-Cryptus <don.cryptus@gmail.com>
2023-05-30 20:49:25 +0200

    client server

---
+++ _chat_server/src/main.rs
@@ -0,0 +1,69 @@
+#![allow(unused_variables)]
+#![allow(unused_assignments)]
+#![allow(unused_mut)]
+#![allow(unused_imports)]
+#![allow(dead_code)]
+
+use core::time::Duration;
+use std::io::{ErrorKind, Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::sync::mpsc;
+use std::thread;
+
+const LOCAL: &str = "127.0.0.1:6000";
+const MSG_SIZE: usize = 32;
+
+fn main() {
+    let server = TcpListener::bind(LOCAL).expect("Listener failed to bind");
+    server
+        .set_nonblocking(true)
+        .expect("Failed to initialize non-blocking");
+    let mut clients = vec![];
+    let (tx, rx) = mpsc::channel::<String>();
+
+    loop {
+        if let Ok((mut socket, addr)) = server.accept() {
+            println!("Client {} connected", addr);
+            let tx = tx.clone();
+            clients.push(socket.try_clone().expect("Failed to clone client"));
+
+            thread::spawn(move || loop {
+                let mut buff = vec![0; MSG_SIZE];
+                match socket.read_exact(&mut buff) {
+                    Ok(_) => {
+                        let msg = buff.into_iter().take_while(|&x| x != 0).collect::<Vec<_>>();
+                        let msg = String::from_utf8(msg).expect("Invalid utf8 message");
+
+                        println!("{}: {:?}", addr, msg);
+
+                        tx.send(msg).expect("Failed to send message to rx");
+                    }
+                    Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+                    Err(_) => {
+                        println!("Closing connection with: {}", addr);
+                        break;
+                    }
+                }
+
+                sleep();
+            });
+        }
+        if let Ok(msg) = rx.try_recv() {
+            clients = clients
+                .into_iter()
+                .filter_map(|mut client| {
+                    let mut buff = msg.clone().into_bytes();
+                    buff.resize(MSG_SIZE, 0);
+
+                    client.write_all(&buff).map(|_| client).ok()
+                })
+                .collect::<Vec<_>>();
+        }
+
+        sleep();
+    }
+}
+
+fn sleep() {
+    thread::sleep(Duration::from_millis(100));
+}
