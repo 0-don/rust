f94400274fd850e5de049675d0410307bd7810f7
Don-Cryptus <don.cryptus@gmail.com>
2023-06-03 00:41:14 +0200

    rusting

--- 2_rust-programming-master-class-from-beginner-to-expert/8 - Advance Concepts/1 - Declarative Macros Basics.rs
+++ 2_rust-programming-master-class-from-beginner-to-expert/8 - Advance Concepts/1 - Declarative Macros Basics.rs
@@ -1,6 +1,6 @@
-    // -------------------------------------------------
-    // 			Declarative Macros 
-    // -------------------------------------------------
+// -------------------------------------------------
+// 			Declarative Macros
+// -------------------------------------------------
 
 /* General Syntax
 
@@ -12,10 +12,9 @@ macro_rules! macro_name {
 }
 */
 
-
-
 macro_rules! our_macro {
-    () => { 1+1;        
+    () => {
+        1 + 1;
     };
 
     (something 4 u dear u32 @_@) => {
@@ -28,28 +27,22 @@ macro_rules! our_macro {
 
     ($a:expr, $b:expr; $c:expr) => {
         $a * ($b + $c)
-    }
+    };
 }
 fn main() {
+    println!("{}", our_macro!());
+    our_macro!();
+    println!("{}", our_macro!(2, 2));
+    println!("{}", our_macro!(5,6;3));
+    // println!("{}", our_macro!("something",2;"nothing"));
 
-    
-    println!("{}",our_macro!()); 
-    our_macro!(); 
-    println!("{}", our_macro!(2,2)); 
-    println!("{}", our_macro!(5,6;3));  
-    // println!("{}", our_macro!("something",2;"nothing"));  
-    
-    our_macro!(); 
-    our_macro![]; 
+    our_macro!();
+    our_macro![];
 
-    our_macro!{};
-
-
+    our_macro! {};
 
     // 1. cargo install cargo-expand
-    // 2. rustup install nightly  
+    // 2. rustup install nightly
     // 3. rustup component add rustfmt
-    // 4. rustup component add rustfmt --toolchain nightly  
-
-
-} 
+    // 4. rustup component add rustfmt --toolchain nightly
+}
